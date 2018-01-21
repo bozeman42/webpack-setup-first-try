cb8f49b4e50be5c904dd177aea87a3a846794074
bozeman42 <bozeman42@gmail.com>
2018-01-21 00:27:05 -0600

    added and configured dev server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+import coolBeans from './modules/module';
+
 console.log('Hello, World!...');
+coolBeans(); coolBeans();
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    stats: 'errors-only',
+    open: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack template generator',
